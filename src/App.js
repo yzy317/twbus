d0e25a54ae0404554194e97da69b6a79cbeb565c
yzy317 <74888284+yzy317@users.noreply.github.com>
2022-08-18 11:58:28 +0800

    -a

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/home";
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Home />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Home from "./pages/Home/home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
